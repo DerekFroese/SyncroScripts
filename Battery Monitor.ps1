d7c042ce156d78edaf69a11faa0c871dbcdda22d
DerekFroese <505350+DerekFroese@users.noreply.github.com>
2022-08-17 08:54:16 -0700

    Update Battery Monitor.ps1
    Fixed and updated Cyberdrain script

--- Battery Monitor.ps1
+++ Battery Monitor.ps1
@@ -1,9 +1,22 @@
+<#
+Monitors battery health by comparing the reported last charge value to the maximum charge value.
+Good to run regularly in a policy.
+
+Set a runtime variable called "AlertPercent". Set your own percentage as an acceptable failure rate. E.g. "50" is 50%
+
+From https://admin.syncromsp.com/shared_scripts/1325
+Updated By Derek Froese, https://www.linkedin.com/in/derekfroese/
+#>
+
+Write-Output "$(get-date -Format 'HH:mm:ss') Starting script"
 Import-Module $env:SyncroModule
+Write-Output "$(get-date -Format 'HH:mm:ss') Running battery report from powercfg"
 & powercfg /batteryreport /XML /OUTPUT "batteryreport.xml"
 start-sleep 3
+
+Write-Output "$(get-date -Format 'HH:mm:ss') processing battery report"   
 [xml]$Report = Get-Content "batteryreport.xml"
-    
-$BatteryStatus = $Report.BatteryReport.Batteries |
+ $BatteryStatus = $Report.BatteryReport.Batteries |
 ForEach-Object {
     [PSCustomObject]@{
         DesignCapacity     = $_.Battery.DesignCapacity
@@ -13,13 +26,23 @@ ForEach-Object {
     }
 }
 
-if (!$BatteryStatus) {
-    Write-Host "This device does not have batteries, or we could not find the status of the batteries."
+if (!$Report.BatteryReport.Batteries) {
+    Write-Output "$(get-date -Format 'HH:mm:ss') This device does not have batteries, or we could not find the status of the batteries."
 }
 
-foreach ($Battery in $BatteryStatus) {
-    if ([int64]$Battery.FullChargeCapacity * 100 / [int64]$Battery.DesignCapacity -gt $AlertPercent) {
-        Rmm-Alert -Category 'Battery_Health' -Body "The battery health is less than expect. The battery was designed for $($battery.DesignCapacity) but the maximum charge is $($Battery.FullChargeCapacity). The battery info is $($Battery.id)"
-
+else {
+    Write-Output $BatteryStatus
+        
+    foreach ($Battery in $BatteryStatus) {
+        $batteryPercent = [int64]$Battery.FullChargeCapacity * 100 / [int64]$Battery.DesignCapacity
+        if ($batteryPercent -lt $AlertPercent) {
+            $message = "Battery Health is $([int]$batteryPercent)% which is below threshold of $($AlertPercent)%. `nThe battery was designed for $($battery.DesignCapacity) but has $($Battery.FullChargeCapacity). `nThe battery info is $($Battery.id)"
+            Write-Output $(get-date -Format "HH:mm:ss") $message
+            Rmm-Alert -Category 'Battery_Health' -Body $message
+    
+        }
     }
 }
+
+Remove-Item "batteryreport.xml"
+Write-Output "$(get-date -Format 'HH:mm:ss') Script complete"   

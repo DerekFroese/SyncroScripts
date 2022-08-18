22e5db5d8d765070001181582a00c15c1f7fc675
Derek Froese <505350+DerekFroese@users.noreply.github.com>
2022-08-18 11:55:41 -0700

    Update Record Bitlocker Keys.ps1

--- Record Bitlocker Keys.ps1
+++ Record Bitlocker Keys.ps1
@@ -11,6 +11,8 @@
     By Derek Froese https://www.linkedin.com/in/derekfroese/
     Modified through discussion with Beau:
     https://www.facebook.com/groups/syncromspusers/posts/1488619958276643/?comment_id=1488706401601332&reply_comment_id=1488889278249711
+    
+    #TODO this script shows as error when run in Syncro even though it runs fine. Could put in a Try/catch for "manage-bde -status"
 #>
 
 Write-Output "$(get-date -Format "HH:mm:ss") Starting Script"   
